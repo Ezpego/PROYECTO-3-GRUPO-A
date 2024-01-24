e5ecc52b77b54cbfbecab1d1e1b198c0eb47d6e2
Gabriel Sastre <gabisas@icloud.com>
2024-01-24 09:10:59 +0100

    se añade contenido hompepage que no se habia salvado

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,26 @@
+import ExercisesList from "../components/ExercisesList/ExercisesList";
+import LoginForm from "../components/LoginForm/LoginForm";
+import TokenContext from "../context/TokenContext";
+import { useContext } from "react";
+import { RxExit } from "react-icons/rx";
+
 const HomePage = () => {
+  const { token, setToken, tokenUpdate, userData, isAdmin } =
+    useContext(TokenContext);
 
-    return (
-        <div>
-            <p>Página principal </p>
-        </div>
-    )
-
-}
+  const handleExitButton = () => {
+    tokenUpdate("");
+  };
+  return (
+    <div>
+      <header>
+        <h1>GymJoy</h1>
+      </header>
+      <p>Página principal </p>
+      {token ? <ExercisesList /> : <LoginForm />}
+      <footer> {token && <RxExit onClick={handleExitButton} />}</footer>
+    </div>
+  );
+};
 
 export default HomePage;
