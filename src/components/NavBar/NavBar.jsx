f667b89d9cb9dcd6ba029ac065d76f9406a444ea
Ezequiel Pérez <ezpego@gmail.com>
2024-02-02 08:17:53 +0100

    limpiando algo de codigo en navbar y profileIcon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,13 @@
-// import { RxExit } from "react-icons/rx";
-// import { useContext } from "react";
-// import TokenContext from "../../context/TokenContext";
 import ProfileIcon from "../ProfileIcon/ProfileIcon";
-import { useNavigate } from "react-router-dom";
+
 
 
 const NavBar = () => {
 
-    const navigate = useNavigate();
-
-
-    const handleViewProfileClick = () => {
-    navigate('/user/:userId/editProfile');  
-    };
-
-
 
 return (
     <>
-        <ProfileIcon
-        onViewProfileClick={handleViewProfileClick}
-        />
+        <ProfileIcon />
 
     </>
 );
--- src/components/ProfileIcon/ProfileIcon.jsx
+++ src/components/ProfileIcon/ProfileIcon.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 import PropTypes from 'prop-types';
 
-const ProfileIcon = ({ onViewProfileClick }) => {
+const ProfileIcon = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const {userData, error, clearError, tokenUpdate} = useContext(TokenContext);
   const navigate = useNavigate();
@@ -17,7 +17,6 @@ const ProfileIcon = ({ onViewProfileClick }) => {
     clearError();
   };
 
-
   const handleExitButton = () => {
     tokenUpdate("");
     navigate('/');
@@ -25,7 +24,7 @@ const ProfileIcon = ({ onViewProfileClick }) => {
 
   const handleViewProfileDetails = () => {
     handleMenuOpen();
-    onViewProfileClick();
+    navigate('/user/:userId/editProfile');  
   }
 
   return (
