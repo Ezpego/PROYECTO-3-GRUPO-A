e2a8a6d8b3b07ba11035e4d9385cbdcab1a0968c
Daniel Pardo Martin <daniel.pardo.martin20@gmail.com>
2024-02-07 12:26:12 +0100

    cambios

--- src/components/ExercisesForm/ExercisesFormCreated.jsx
+++ src/components/ExercisesForm/ExercisesFormCreated.jsx
@@ -4,228 +4,231 @@ import { useState, useContext } from "react";
 import { llamadaServidor } from "../../utils/llamadasServidor";
 import TokenContext from "../../context/TokenContext";
 
-const FormExercises = ({ onSubmit }) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm();
+const FormExercises = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm();
 
-  const [data, setData] = useState(null);
-  const [imageFile, setImageFile] = useState(null);
-  const [previewSrc, setPreviewSrc] = useState("");
-  const { token } = useContext(TokenContext);
+    const [data, setData] = useState(null);
+    const [imageFile, setImageFile] = useState(null);
+    const [previewSrc, setPreviewSrc] = useState("");
+    const { token } = useContext(TokenContext);
 
-  useEffect(() => {
-    const filtros = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/filter", {
-          method: "GET",
-          headers: {
-            authorization: `${token}`,
-          },
-        });
-        const result = await response.json();
-        console.log(result);
-        setData(result);
-      } catch (error) {
-        console.error("Error. ", error);
-      }
-      return data;
+    useEffect(() => {
+        const filtros = async () => {
+            try {
+                const response = await fetch("http://localhost:3000/filter", {
+                    method: "GET",
+                    headers: {
+                        authorization: `${token}`,
+                    },
+                });
+                const result = await response.json();
+                console.log(result);
+                setData(result);
+            } catch (error) {
+                console.error("Error. ", error);
+            }
+            return data;
+        };
+        filtros();
+    }, []);
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        setImageFile(file);
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setPreviewSrc(reader.result);
+        };
+        reader.readAsDataURL(file);
     };
-    filtros();
-  }, []);
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    setImageFile(file);
+    const onSubmit = (data) => {
+        console.log("Datos: ", data);
+        console.log(data.photo[0]);
+        const sendExercice = async () => {
+            const urlObjetivo = "/exercises";
+            const opciones = {
+                authorization: `${token}`,
+            };
+            const formDataToSend = new FormData();
+            formDataToSend.append("photo", data.photo[0]);
+            Object.entries(data).forEach(([key, value]) => {
+                if (key !== "photo") {
+                    formDataToSend.append(key, value);
+                }
+            });
 
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      setPreviewSrc(reader.result);
+            const result = await llamadaServidor(
+                urlObjetivo,
+                "POST",
+                opciones,
+                formDataToSend
+            );
+
+            console.log(result);
+        };
+
+        sendExercice();
+        reset();
+        setPreviewSrc();
     };
-    reader.readAsDataURL(file);
-  };
 
-  // onSubmit = (data) => {
-  //     console.log("Datos: ", data);
-  //     console.log(data.photo[0]);
-  //     const sendExercice = async () => {
-  //         const urlObjetivo = "/exercises";
-  //         const opciones = {
-  //             authorization:
-  //                 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IlRlcm1pbmF0b3IiLCJpc0FkbWluaXN0cmF0b3IiOjEsImlhdCI6MTcwNjk4NjM1MiwiZXhwIjoxNzA3NTkxMTUyfQ.OB9EH7Y3qzGQlgFvgTQwzwyEDiJEoiwwzvSMZOWxt3k",
-  //         };
-  //         const formDataToSend = new FormData();
-  //         formDataToSend.append("photo", data.photo[0]);
-  //         Object.entries(data).forEach(([key, value]) => {
-  //             if (key !== "photo") {
-  //                 formDataToSend.append(key, value);
-  //             }
-  //         });
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>
+                    Nombre
+                    <input
+                        type="text"
+                        {...register("name", { required: true })}
+                        maxLength={30}
+                    />
+                </label>
+                {errors.name && (
+                    <span
+                        style={{
+                            color: "red",
+                            fontSize: "0.8em",
+                            marginRight: "5px",
+                        }}
+                    >
+                        Este campo es obligatorio
+                    </span>
+                )}
 
-  //         const result = await llamadaServidor(
-  //             urlObjetivo,
-  //             "POST",
-  //             opciones,
-  //             formDataToSend
-  //         );
+                <label>
+                    Descripcion
+                    <textarea
+                        {...register("description", { required: true })}
+                    />
+                </label>
+                {errors.description && (
+                    <span
+                        style={{
+                            color: "red",
+                            fontSize: "0.8em",
+                            marginRight: "5px",
+                        }}
+                    >
+                        Este campo es obligatorio
+                    </span>
+                )}
 
-  //         console.log(result);
-  //     };
+                <label htmlFor="difficulty_level">Nivel de dificultad</label>
+                <select
+                    id="difficulty_level"
+                    name="difficulty_level"
+                    {...register("difficulty_level", { required: true })}
+                >
+                    <option value="">Nada seleccionado</option>
+                    {data &&
+                        data.difficulty_level.map((item, index) => {
+                            return (
+                                <option key={index} value={item}>
+                                    {item}
+                                </option>
+                            );
+                        })}
+                </select>
+                {errors.difficulty_level && (
+                    <span
+                        style={{
+                            color: "red",
+                            fontSize: "0.8em",
+                            marginRight: "5px",
+                        }}
+                    >
+                        Este campo es obligatorio
+                    </span>
+                )}
 
-  //     sendExercice();
-  //     reset();
-  //     setPreviewSrc();
-  // };
+                <label htmlFor="muscle_group">Grupo muscular</label>
+                <select
+                    id="muscle_group"
+                    name="muscle_group"
+                    {...register("muscle_group", { required: true })}
+                >
+                    <option value="">Nada seleccionado</option>
+                    {data &&
+                        data.muscle.map((item) => (
+                            <option key={item.id} id={item.id} value={item.id}>
+                                {item.name}
+                            </option>
+                        ))}
+                </select>
+                {errors.muscle_group && (
+                    <span
+                        style={{
+                            color: "red",
+                            fontSize: "0.8em",
+                            marginRight: "5px",
+                        }}
+                    >
+                        Este campo es obligatorio
+                    </span>
+                )}
 
-  return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          Nombre
-          <input
-            type="text"
-            {...register("name", { required: true })}
-            maxLength={30}
-          />
-        </label>
-        {errors.name && (
-          <span
-            style={{
-              color: "red",
-              fontSize: "0.8em",
-              marginRight: "5px",
-            }}
-          >
-            Este campo es obligatorio
-          </span>
-        )}
+                <select
+                    id="typology"
+                    name="typology"
+                    {...register("typology", { required: true })}
+                >
+                    <option value="">Nada seleccionado</option>
+                    {data &&
+                        data.typology.map((item) => (
+                            <option key={item.id} id={item.id} value={item.id}>
+                                {item.name}
+                            </option>
+                        ))}
+                </select>
+                {errors.typology && (
+                    <span
+                        style={{
+                            color: "red",
+                            fontSize: "0.8em",
+                            marginRight: "5px",
+                        }}
+                    >
+                        Este campo es obligatorio
+                    </span>
+                )}
 
-        <label>
-          Descripcion
-          <textarea {...register("description", { required: true })} />
-        </label>
-        {errors.description && (
-          <span
-            style={{
-              color: "red",
-              fontSize: "0.8em",
-              marginRight: "5px",
-            }}
-          >
-            Este campo es obligatorio
-          </span>
-        )}
+                <input
+                    type="file"
+                    {...register("photo", { required: true })}
+                    onChange={handleImageChange}
+                />
+                {errors.photo && (
+                    <span
+                        style={{
+                            color: "red",
+                            fontSize: "0.8em",
+                            marginRight: "5px",
+                        }}
+                    >
+                        Este campo es obligatorio
+                    </span>
+                )}
+                {previewSrc && (
+                    <img src={previewSrc} alt="preview" width={"200px"} />
+                )}
 
-        <label htmlFor="difficulty_level">Nivel de dificultad</label>
-        <select
-          id="difficulty_level"
-          name="difficulty_level"
-          {...register("difficulty_level", { required: true })}
-        >
-          <option value="">Nada seleccionado</option>
-          {data &&
-            data.difficulty_level.map((item, index) => {
-              return (
-                <option key={index} value={item}>
-                  {item}
-                </option>
-              );
-            })}
-        </select>
-        {errors.difficulty_level && (
-          <span
-            style={{
-              color: "red",
-              fontSize: "0.8em",
-              marginRight: "5px",
-            }}
-          >
-            Este campo es obligatorio
-          </span>
-        )}
-
-        <label htmlFor="muscle_group">Grupo muscular</label>
-        <select
-          id="muscle_group"
-          name="muscle_group"
-          {...register("muscle_group", { required: true })}
-        >
-          <option value="">Nada seleccionado</option>
-          {data &&
-            data.muscle.map((item) => (
-              <option key={item.id} id={item.id} value={item.id}>
-                {item.name}
-              </option>
-            ))}
-        </select>
-        {errors.muscle_group && (
-          <span
-            style={{
-              color: "red",
-              fontSize: "0.8em",
-              marginRight: "5px",
-            }}
-          >
-            Este campo es obligatorio
-          </span>
-        )}
-
-        <select
-          id="typology"
-          name="typology"
-          {...register("typology", { required: true })}
-        >
-          <option value="">Nada seleccionado</option>
-          {data &&
-            data.typology.map((item) => (
-              <option key={item.id} id={item.id} value={item.id}>
-                {item.name}
-              </option>
-            ))}
-        </select>
-        {errors.typology && (
-          <span
-            style={{
-              color: "red",
-              fontSize: "0.8em",
-              marginRight: "5px",
-            }}
-          >
-            Este campo es obligatorio
-          </span>
-        )}
-
-        <input
-          type="file"
-          {...register("photo", { required: true })}
-          onChange={handleImageChange}
-        />
-        {errors.photo && (
-          <span
-            style={{
-              color: "red",
-              fontSize: "0.8em",
-              marginRight: "5px",
-            }}
-          >
-            Este campo es obligatorio
-          </span>
-        )}
-        {previewSrc && <img src={previewSrc} alt="preview" width={"200px"} />}
-
-        <button type="submit">Enviar</button>
-        <button
-          onClick={() => window.location.reload()}
-          style={{ color: "red", marginLeft: "50px" }}
-        >
-          Volver
-        </button>
-      </form>
-    </>
-  );
+                <button type="submit">Enviar</button>
+                <button
+                    onClick={() => window.location.reload()}
+                    style={{ color: "red", marginLeft: "50px" }}
+                >
+                    Volver
+                </button>
+            </form>
+        </>
+    );
 };
 
 export default FormExercises;
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -19,50 +19,56 @@ import UserGestion from "../components/UserGestion/UserGestion";
 import { useState } from "react";
 
 const AdminPage = () => {
-  const { state } = useLocation();
-  console.log(state);
-  const [currentComponent, setCurrentComponent] = useState(null);
+    const { state } = useLocation();
+    console.log(state);
+    const [currentComponent, setCurrentComponent] = useState(null);
 
-  const buttonStyle = {
-    border: "1px solid #ccc",
-    padding: "20px 20px",
-    borderRadius: "15px",
-    cursor: "pointer",
-    margin: "20px",
-  };
+    const buttonStyle = {
+        border: "1px solid #ccc",
+        padding: "20px 20px",
+        borderRadius: "15px",
+        cursor: "pointer",
+        margin: "20px",
+    };
 
-  return (
-    <>
-      {currentComponent === "FormExercises" && <FormExercises />}
-      {currentComponent === "UserGestion" && <UserGestion />}
-      {currentComponent === "FormExercisesEditer" && <FormExercisesEditer />}
+    return (
+        <>
+            {currentComponent === "FormExercises" && <FormExercises />}
+            {currentComponent === "UserGestion" && <UserGestion />}
+            {currentComponent === "FormExercisesEditer" && (
+                <FormExercisesEditer defaultValues={state} />
+            )}
 
-      {!currentComponent && (
-        <div style={{ textAlign: "center", marginTop: "20px" }}>
-          <h1 style={{ textAlign: "center", fontSize: "1.5em" }}>Admin Page</h1>
-          <button
-            style={buttonStyle}
-            onClick={() => setCurrentComponent("FormExercises")}
-          >
-            Formulario nuevo ejercicio
-          </button>
+            {!currentComponent && (
+                <div style={{ textAlign: "center", marginTop: "20px" }}>
+                    <h1 style={{ textAlign: "center", fontSize: "1.5em" }}>
+                        Admin Page
+                    </h1>
+                    <button
+                        style={buttonStyle}
+                        onClick={() => setCurrentComponent("FormExercises")}
+                    >
+                        Formulario nuevo ejercicio
+                    </button>
 
-          <button
-            style={buttonStyle}
-            onClick={() => setCurrentComponent("UserGestion")}
-          >
-            Gestión de usuarios
-          </button>
-          <button
-            style={buttonStyle}
-            onClick={() => setCurrentComponent("FormExercisesEditer")}
-          >
-            Edicion de ejercicios{" "}
-          </button>
-        </div>
-      )}
-    </>
-  );
+                    <button
+                        style={buttonStyle}
+                        onClick={() => setCurrentComponent("UserGestion")}
+                    >
+                        Gestión de usuarios
+                    </button>
+                    <button
+                        style={buttonStyle}
+                        onClick={() =>
+                            setCurrentComponent("FormExercisesEditer")
+                        }
+                    >
+                        Edicion de ejercicios{" "}
+                    </button>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default AdminPage;
