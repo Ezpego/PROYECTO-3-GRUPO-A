8fa5218fae9d0004a73f17c3cd124a4b252e8440
Hugo vercher <hugovercher80@gmail.com>
2024-02-09 13:20:19 +0100

    ajuste navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,52 @@
+// import ButtonsHome from "../Buttons/ButtonsHome/ButtonsHome";
+// import ModalButton from "../ModalButton/ModalButton";
+// import ProfileIcon from "../ProfileIcon/ProfileIcon";
+// import TokenContext from "../../context/TokenContext";
+// import { useContext } from "react";
+// import { Link } from "react-router-dom";
+// import { SiAddthis } from "react-icons/si";
+
+// const NavBar = () => {
+//   const { isAdmin } = useContext(TokenContext);
+
+//   return (
+//     <>
+//       <span className="navContainer">
+//         {" "}
+//         <ProfileIcon />
+//         <ModalButton />
+//         {isAdmin && (
+//           <Link to="/exercises/">
+//             <SiAddthis />
+//           </Link>
+//         )}
+//         <ButtonsHome />
+//       </span>
+//     </>
+//   );
+// };
+
+// export default NavBar;
+
+import { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { SiAddthis } from "react-icons/si";
 import ButtonsHome from "../Buttons/ButtonsHome/ButtonsHome";
 import ModalButton from "../ModalButton/ModalButton";
 import ProfileIcon from "../ProfileIcon/ProfileIcon";
 import TokenContext from "../../context/TokenContext";
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { SiAddthis } from "react-icons/si";
 
 const NavBar = () => {
   const { isAdmin } = useContext(TokenContext);
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
 
   return (
     <>
       <span className="navContainer">
-        {" "}
         <ProfileIcon />
-        <ModalButton />
-        {isAdmin && (
+        {isHomePage && <ModalButton />}
+        {isAdmin && !isHomePage && (
           <Link to="/exercises/">
             <SiAddthis />
           </Link>
