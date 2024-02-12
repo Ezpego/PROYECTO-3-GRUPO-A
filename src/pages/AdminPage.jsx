75a261d8c911c037f91f6d1beb35c25ebb7f9565
Hugo vercher <hugovercher80@gmail.com>
2024-02-12 13:53:49 +0100

    avance css

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -4,47 +4,45 @@ import UserGestion from "../components/UserGestion/UserGestion";
 import { useState } from "react";
 
 const AdminPage = () => {
-    const { state } = useLocation();
-    console.log(state);
-    const [currentComponent, setCurrentComponent] = useState(null);
+  const { state } = useLocation();
+  console.log(state);
+  const [currentComponent, setCurrentComponent] = useState(null);
 
-    const buttonStyle = {
-        border: "1px solid #ccc",
-        padding: "20px 20px",
-        borderRadius: "15px",
-        cursor: "pointer",
-        margin: "20px",
-    };
+  const buttonStyle = {
+    border: "1px solid #ccc",
+    padding: "20px 20px",
+    borderRadius: "15px",
+    cursor: "pointer",
+    margin: "20px",
+  };
 
-    return (
-        <>
-            {currentComponent === "FormExercises" && <FormExercises />}
-            {currentComponent === "UserGestion" && (
-                <UserGestion setCurrentComponent={setCurrentComponent} />
-            )}
+  return (
+    <>
+      {currentComponent === "FormExercises" && <FormExercises />}
+      {currentComponent === "UserGestion" && (
+        <UserGestion setCurrentComponent={setCurrentComponent} />
+      )}
 
-            {!currentComponent && (
-                <div style={{ textAlign: "center", marginTop: "20px" }}>
-                    <h1 style={{ textAlign: "center", fontSize: "1.5em" }}>
-                        Admin Page
-                    </h1>
-                    <button
-                        style={buttonStyle}
-                        onClick={() => setCurrentComponent("FormExercises")}
-                    >
-                        Formulario nuevo ejercicio
-                    </button>
+      {!currentComponent && (
+        <div style={{ textAlign: "center" }}>
+          <h1 style={{ textAlign: "center", fontSize: "1.5em" }}>Admin Page</h1>
+          <button
+            style={buttonStyle}
+            onClick={() => setCurrentComponent("FormExercises")}
+          >
+            Formulario nuevo ejercicio
+          </button>
 
-                    <button
-                        style={buttonStyle}
-                        onClick={() => setCurrentComponent("UserGestion")}
-                    >
-                        Gestión de usuarios
-                    </button>
-                </div>
-            )}
-        </>
-    );
+          <button
+            style={buttonStyle}
+            onClick={() => setCurrentComponent("UserGestion")}
+          >
+            Gestión de usuarios
+          </button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default AdminPage;
